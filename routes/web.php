28348fdb986c060da70aa1445afad04a2adff53d
Dylan Chan <N00173835@iadt.ie>
2019-10-07 18:55:07 +0100

    MoToDo

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+# @Date:   2019-10-07T18:19:56+01:00
+# @Last modified time: 2019-10-07T18:40:53+01:00
+
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+
+});
+
+Route::get('helloworld', function () {
+    return '<h1>Helloworld<h1>';
+
+});
